6133b34bcd0c475c2aab6a9e84ef0d1d8b7d16e9
Pedro Filipe <pedro@adeven.com>
2014-05-09 18:01:15 +0200

    Created Windows native bridge projects

---
+++ Assets/Plugins/Metro/src/AdjustUnityWS/RealDLL/AdjustWS.cs
@@ -0,0 +1,69 @@
+﻿using AdjustSdk;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace AdjustUnityWS
+{
+    public class AdjustWS
+    {
+        public static void AppDidLaunch(string appToken, string sLogLevel, string sEnvironment)
+        {
+            Adjust.AppDidLaunch(appToken);
+            var logLevel = AdjustSdk.ActivityKindUtil.FromString(sLogLevel);
+            Adjust.SetLogLevel((LogLevel)logLevel);
+
+            if ("sandbox" == sEnvironment)
+                Adjust.SetEnvironment(AdjustEnvironment.Sandbox);
+
+            if ("production" == sEnvironment)
+                Adjust.SetEnvironment(AdjustEnvironment.Production);
+        }
+
+        public static void AppDidActivate()
+        {
+            Adjust.AppDidActivate();
+        }
+
+        public static void AppDidDeactivate()
+        {
+            Adjust.AppDidDeactivate();
+        }
+
+        public static void TrackEvent(string eventToken, Dictionary<string, string> callbackParameters = null)
+        {
+            Adjust.TrackEvent(eventToken, callbackParameters);
+        }
+
+        public static void TrackRevenue(double amountInCents, string eventToken = null, Dictionary<string, string> callbackParameters = null)
+        {
+            Adjust.TrackRevenue(amountInCents, eventToken, callbackParameters);
+        }
+
+        public static void SetEnabled(bool enabled)
+        {
+            Adjust.SetEnabled(enabled);
+        }
+
+        public static bool IsEnabled()
+        {
+            return Adjust.IsEnabled();
+        }
+
+        /// <summary>
+        /// Sets the response delegate that takes a ResponseData from a response delegate that takes a Json string
+        /// </summary>
+        /// <param name="responseDelegate">Response delegate that takes a Json string</param>
+        public static void SetResponseDelegateString(Action<string> responseDelegate)
+        {
+            // function to convert a ResponseData object to a Json String
+            Func<ResponseData, string> convertResponseDataToString = (responseData => JsonConvert.SerializeObject(responseData.ToDic()));
+
+            // Convert the response delegate from taking a string, to taking a ResponseData using a function that converts from ResponseData to string
+            Action<ResponseData> responseDelegateConverted = (responseData => responseDelegate(convertResponseDataToString(responseData)));
+
+            // Sets the converted response delegate that takes the ResponseData
+            Adjust.SetResponseDelegate(responseDelegateConverted);
+        }
+    }
+}
---
+++ Assets/Plugins/WP8/src/AdjustUnityWP/FakeDLL/AdjustWP.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdjustUnityWP
+{
+    public class AdjustWP
+    {
+        public static void AppDidLaunch(string appToken, string sLogLevel, string sEnvironment) { }
+
+        public static void AppDidActivate() { }
+
+        public static void AppDidDeactivate() { }
+
+        public static void TrackEvent(string eventToken, Dictionary<string, string> callbackParameters = null) { }
+
+        public static void TrackRevenue(double amountInCents, string eventToken = null, Dictionary<string, string> callbackParameters = null) { }
+
+        public static void SetEnabled(bool enabled) { }
+
+        public static bool IsEnabled()
+        {
+            return false;
+        }
+
+        public static void SetResponseDelegateString(Action<string> responseDelegate) { }
+    }
+}
