ac9217bfd3e5173b059e09669e2c7195deafe355
uerceg <ugljesa@adjust.com>
2015-08-26 17:23:20 +0200

    Improving Android API

--- Assets/Adjust/Android/AdjustAndroid.cs
+++ Assets/Adjust/Android/AdjustAndroid.cs
@@ -34,6 +34,7 @@ namespace com.adjust.sdk
 				adjustAttribution.campaign = attribution.Get<string> ("campaign");
 				adjustAttribution.adgroup = attribution.Get<string> ("adgroup");
 				adjustAttribution.creative = attribution.Get<string> ("creative");
+				adjustAttribution.clickLabel = attribution.Get<string> ("clickLabel");
 
 				if (callback != null) {
 					callback (adjustAttribution);
@@ -132,6 +133,16 @@ namespace com.adjust.sdk
 		public void setOfflineMode (bool enabled)
 		{
 			ajcAdjust.CallStatic ("setOfflineMode", enabled);
+		}
+
+		public void setReferrer(string referrer)
+		{
+			ajcAdjust.CallStatic ("setReferrer", referrer);
+		}
+
+		public void setDeviceToken(string deviceToken)
+		{
+
 		}
 
 		#endregion
