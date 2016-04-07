92d03b4f3d03dbb3afa4c7960b47c5bb583acfc3
Pedro <pedro@adeven.com>
2016-04-07 15:31:38 +0200

    Refactor Json utils

--- Assets/Adjust/Unity/AdjustUtils.cs
+++ Assets/Adjust/Unity/AdjustUtils.cs
@@ -80,8 +80,25 @@ namespace com.adjust.sdk
 
         public static void PrintJsonResponse (Dictionary<string, object> dictionary)
         {
+			if (dictionary == null) {
+				return;
+			}
+
             foreach (KeyValuePair<string, object> pair in dictionary)
             {
+				String valueString = pair.Value as string;
+
+				if (valueString != null) {
+					Debug.Log ("Key = " + pair.Key);
+					Debug.Log ("Value = " + valueString);
+					continue;
+				}
+
+				Dictionary<string, object> valueDictionary = pair.Value as Dictionary<string, object>;
+
+				PrintJsonResponse (valueDictionary);
+
+				/*
                 Type t = pair.Value.GetType ();
                 bool isDict = t.IsGenericType && t.GetGenericTypeDefinition () == typeof(Dictionary<,>);
 
@@ -94,42 +111,50 @@ namespace com.adjust.sdk
                     Debug.Log ("Key = " + pair.Key);
                     Debug.Log ("Value = " + pair.Value);
                 }
+                */
             }
         }
 
         public static String GetJsonString (JSONNode node, string key)
         {
-            var jsonValue = GetJsonValue (node, key);
-            
-            if (jsonValue == null)
-            {
-                return null;
-            }
-            
-            return jsonValue.Value;
-        }
-        
-        public static JSONNode GetJsonValue (JSONNode node, string key)
-        {
-            if (node == null)
-            {
-                return null;
-            }
-            
-            var nodeValue = node [key];
+			if (node == null)
+			{
+				return null;
+			}
 
-            if (nodeValue.GetType () == typeof (JSONLazyCreator))
+			// access value object and cast it to JSONData
+			var nodeValue = node [key] as JSONData;
+
+			if (nodeValue == null)
             {
                 return null;
             }
-            
-            return nodeValue;
+
+			return nodeValue.Value;
         }
 
         public static void WriteJsonResponseDictionary (JSONClass jsonObject, Dictionary<string, object> output)
         {
             foreach (KeyValuePair<string, JSONNode> pair in jsonObject)
             {
+				// try to cast value as a complex object
+				var subNode = pair.Value.AsObject;
+				var key = pair.Key;
+
+				// value is not a complex object
+				if (subNode == null) {
+					var value = pair.Value.Value;
+					output.Add (key, value);
+					continue;
+				}
+
+				// create new dictionary for complex type
+				var newSubDictionary = new Dictionary<string, object> ();
+				// save it in the current dictionary
+				output.Add (key, newSubDictionary);
+				// recursive call to fill new dictionary
+				WriteJsonResponseDictionary(subNode, newSubDictionary);
+				/*
                 if (pair.Value.AsObject == null)
                 {
                     output.Add (pair.Key, pair.Value);
@@ -139,6 +164,7 @@ namespace com.adjust.sdk
                     output.Add (pair.Key, new Dictionary<string, object> ());
                     WriteJsonResponseDictionary (pair.Value.AsObject, (Dictionary<string, object>)output [pair.Key]);
                 }
+                */
             }
         }
         #endregion
