052cea13c19f387ee6b5a1d3ff087935ec409120
uerceg <ugi@adjust.com>
2017-03-24 17:27:52 +0100

    Unity 5 package restructuring

--- Assets/Adjust/Metro/AdjustMetro.cs
+++ Assets/Adjust/Metro/AdjustMetro.cs
@@ -11,7 +11,7 @@ using AdjustUnityWS81;
 #endif
 namespace com.adjust.sdk {
     public class AdjustMetro : IAdjust {
-        private const string sdkPrefix = "unity4.11.1";
+        private const string sdkPrefix = "unity4.11.0";
 
         public bool isEnabled() {
 #if UNITY_WSA_10_0
