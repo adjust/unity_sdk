3933723244e56a37598cbe89d113016c6cfd17a7
serj <srdjan@adjust.com>
2017-11-13 18:30:39 +0100

    added support for Win8.1 & WinWS; added support for deferred deeplink callback

--- Assets/Adjust/Windows/AdjustWindows.cs
+++ Assets/Adjust/Windows/AdjustWindows.cs
@@ -7,65 +7,44 @@ using UnityEngine;
 
 #if UNITY_WSA_10_0
 using Win10Interface;
-#elif UNITY_WINRT_8_1
+#elif UNITY_WP_8_1
 using Win81Interface;
+#elif UNITY_WSA
+using WinWsInterface;
 #endif
 
 namespace com.adjust.sdk {
     public class AdjustWindows : IAdjust {
         private const string sdkPrefix = "unity4.12.0";
 
-		// TODO: make AdjustWS10 & AdjusWS81 implement an interface, which can be used as an abstraction here
-		// thus leading to less compiler branching (e.g. #if, #endif, etc.)
-
         public bool isEnabled() {
-#if UNITY_WSA_10_0
-            return AdjustWS10.IsEnabled();
-#elif UNITY_WINRT_8_1
-            return AdjustWS81.IsEnabled();
-#endif
+			return AdjustWinInterface.IsEnabled();
         }
 
         public string getAdid() {
-			return AdjustWS10.GetAdid ();
+			return AdjustWinInterface.GetAdid ();
         }
 
         public AdjustAttribution getAttribution() {
-			var attribution = AdjustWS10.GetAttribution ();
+			var attribution = AdjustWinInterface.GetAttribution ();
 			var attributionJson = JsonUtility.ToJson (attribution);
 			return new AdjustAttribution (attributionJson);
         }
 
         public void onPause() {
-#if UNITY_WSA_10_0
-            AdjustWS10.ApplicationDeactivated();
-#elif UNITY_WINRT_8_1
-            AdjustWS81.ApplicationDeactivated();
-#endif
+			AdjustWinInterface.ApplicationDeactivated();
         }
 
         public void onResume() {
-#if UNITY_WSA_10_0
-            AdjustWS10.ApplicationActivated();
-#elif UNITY_WINRT_8_1
-            AdjustWS81.ApplicationActivated();
-#endif
+			AdjustWinInterface.ApplicationActivated();
         }
 
         public void setEnabled(bool enabled) {
-#if UNITY_WSA_10_0
-            AdjustWS10.SetEnabled(enabled);
-#elif UNITY_WINRT_8_1
-            AdjustWS81.SetEnabled(enabled);
-#endif
+			AdjustWinInterface.SetEnabled(enabled);
         }
 
         public void setOfflineMode(bool offlineMode) {
-#if UNITY_WSA_10_0
-            AdjustWS10.SetOfflineMode(offlineMode);
-#elif UNITY_WINRT_8_1
-            AdjustWS81.SetOfflineMode(offlineMode);
-#endif
+			AdjustWinInterface.SetOfflineMode(offlineMode);
         }
 
         public void start(AdjustConfig adjustConfig) {
@@ -76,6 +55,7 @@ namespace com.adjust.sdk {
 			Action<Dictionary<string, string>> sessionFailureChangedAction = null;
 			Action<Dictionary<string, string>> eventSuccessChangedAction = null;
 			Action<Dictionary<string, string>> eventFailureChangedAction = null;
+			Func<string, bool> deeplinkResponseFunc = null;
 
             if (adjustConfig.logLevel.HasValue) {
                 logLevelString = lowercaseToString(adjustConfig.logLevel.Value);
@@ -116,6 +96,16 @@ namespace com.adjust.sdk {
 				};
 			}
 
+			if (adjustConfig.deeplinkDelegate != null) {
+				deeplinkResponseFunc = uri => {
+					if(adjustConfig.launchDeferredDeeplink) {
+						Adjust.GetNativeDeferredDeeplink(uri);
+					}
+
+					return adjustConfig.launchDeferredDeeplink;
+				};
+			}
+
 			bool sendInBackground = false;
 			if (adjustConfig.sendInBackground.HasValue) {
 				sendInBackground = adjustConfig.sendInBackground.Value;
@@ -142,24 +132,15 @@ namespace com.adjust.sdk {
 				ActionSessionSuccessData = sessionSuccessChangedAction,
 				ActionSessionFailureData = sessionFailureChangedAction,
 				ActionEventSuccessData = eventSuccessChangedAction,
-				ActionEventFailureData = eventFailureChangedAction
+				ActionEventFailureData = eventFailureChangedAction,
+				FuncDeeplinkResponseData = deeplinkResponseFunc
 			};
 
-#if UNITY_WSA_10_0
-			AdjustWS10.ApplicationLaunching (
-#elif UNITY_WINRT_8_1
-            AdjustWS81.ApplicationLaunching(
-#endif
-				adjustConfigDto
-			);
+			AdjustWinInterface.ApplicationLaunching (adjustConfigDto);
         }
 
         public void trackEvent(AdjustEvent adjustEvent) {
-#if UNITY_WSA_10_0
-			AdjustWS10.TrackEvent (
-#elif UNITY_WINRT_8_1
-            AdjustWS81.TrackEvent(
-#endif
+			AdjustWinInterface.TrackEvent (
 				eventToken: adjustEvent.eventToken,
 				revenue: adjustEvent.revenue,
 				currency: adjustEvent.currency,
@@ -170,39 +151,39 @@ namespace com.adjust.sdk {
         }
 
         public void sendFirstPackages() {
-			AdjustWS10.SendFirstPackages ();
+			AdjustWinInterface.SendFirstPackages ();
 		}
 
         public void setDeviceToken(string deviceToken) {
-			AdjustWS10.SetDeviceToken (deviceToken);
+			AdjustWinInterface.SetDeviceToken (deviceToken);
 		}
 
 		public string getWinAdid() {
-			return AdjustWS10.GetWindowsAdId();
+			return AdjustWinInterface.GetWindowsAdId();
 		}
 
         public static void addSessionPartnerParameter(string key, string value) {
-			AdjustWS10.AddSessionPartnerParameter (key, value);
+			AdjustWinInterface.AddSessionPartnerParameter (key, value);
 		}
 
         public static void addSessionCallbackParameter(string key, string value) {
-			AdjustWS10.AddSessionCallbackParameter (key, value);
+			AdjustWinInterface.AddSessionCallbackParameter (key, value);
 		}
 
         public static void removeSessionPartnerParameter(string key) {
-			AdjustWS10.RemoveSessionPartnerParameter (key);
+			AdjustWinInterface.RemoveSessionPartnerParameter (key);
 		}
 
         public static void removeSessionCallbackParameter(string key) {
-			AdjustWS10.RemoveSessionCallbackParameter (key);
+			AdjustWinInterface.RemoveSessionCallbackParameter (key);
 		}
 
         public static void resetSessionPartnerParameters() {
-			AdjustWS10.ResetSessionPartnerParameters ();
+			AdjustWinInterface.ResetSessionPartnerParameters ();
 		}
 
         public static void resetSessionCallbackParameters() {
-			AdjustWS10.ResetSessionCallbackParameters ();
+			AdjustWinInterface.ResetSessionCallbackParameters ();
 		}
 
 		public static string lowercaseToString(AdjustLogLevel AdjustLogLevel)
