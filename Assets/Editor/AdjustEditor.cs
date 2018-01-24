8a49091404688f1e992d60db599a386083a5c902
uerceg <ugi@adjust.com>
2018-01-24 11:49:52 +0100

    AdjustEditor cleanup

--- Assets/Editor/AdjustEditor.cs
+++ Assets/Editor/AdjustEditor.cs
@@ -12,40 +12,48 @@ using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
 #endif
 
-public class AdjustEditor {
+public class AdjustEditor
+{
     private static bool isPostProcessingEnabled = true;
 
     [MenuItem ("Assets/Adjust/Check post processing status")]
-    public static void CheckPostProcessingPermission() {
+    public static void CheckPostProcessingPermission()
+    {
         EditorUtility.DisplayDialog ("adjust", "The post processing for adjust is " + (isPostProcessingEnabled ? "enabled." : "disabled."), "OK");
     }
 
     [MenuItem ("Assets/Adjust/Change post processing status")]
-    public static void ChangePostProcessingPermission() {
+    public static void ChangePostProcessingPermission()
+    {
         isPostProcessingEnabled = !isPostProcessingEnabled;
         EditorUtility.DisplayDialog ("adjust", "The post processing for adjust is now " + (isPostProcessingEnabled ? "enabled." : "disabled."), "OK");
     }
 
     [PostProcessBuild]
-    public static void OnPostprocessBuild(BuildTarget target, string projectPath) {
+    public static void OnPostprocessBuild(BuildTarget target, string projectPath)
+    {
         // Check what is user setting about allowing adjust SDK to perform post build tasks.
         // If user disabled it, oh well, we won't do a thing.
-        if (!isPostProcessingEnabled) {
+        if (!isPostProcessingEnabled)
+        {
             UnityEngine.Debug.Log("adjust: You have forbidden the adjust SDK to perform post processing tasks.");
             UnityEngine.Debug.Log("adjust: Skipping post processing tasks.");
-
             return;
         }
 
         RunPostBuildScript(target:target, preBuild:false, projectPath:projectPath);
     }
 
-    private static void RunPostBuildScript(BuildTarget target, bool preBuild, string projectPath = "") {
-        if (target == BuildTarget.Android) {
+    private static void RunPostBuildScript(BuildTarget target, bool preBuild, string projectPath = "")
+    {
+        if (target == BuildTarget.Android)
+        {
             UnityEngine.Debug.Log("adjust: Starting to perform post build tasks for Android platform.");
             RunPostProcessTasksAndroid();
-        } else if (target == BuildTarget.iOS) {
-            #if UNITY_IOS
+        }
+        else if (target == BuildTarget.iOS)
+        {
+#if UNITY_IOS
             UnityEngine.Debug.Log("adjust: Starting to perform post build tasks for iOS platform.");
             
             string xcodeProjectPath = projectPath + "/Unity-iPhone.xcodeproj/project.pbxproj";
@@ -85,15 +93,14 @@ public class AdjustEditor {
 
             // Save the changes to Xcode project file.
             xcodeProject.WriteToFile(xcodeProjectPath);
-            #endif
+#endif
         }
     }
 
-    private static void RunPostProcessTasksiOS(string projectPath) {
-        
-    }
+    private static void RunPostProcessTasksiOS(string projectPath) {}
 
-    private static void RunPostProcessTasksAndroid() {
+    private static void RunPostProcessTasksAndroid()
+    {
         bool isAdjustManifestUsed = false;
         string androidPluginsPath = Path.Combine(Application.dataPath, "Plugins/Android");
         string adjustManifestPath = Path.Combine(Application.dataPath, "Adjust/Android/AdjustAndroidManifest.xml");
@@ -101,23 +108,28 @@ public class AdjustEditor {
 
         // Check if user has already created AndroidManifest.xml file in its location.
         // If not, use already predefined AdjustAndroidManifest.xml as default one.
-        if (!File.Exists(appManifestPath)) {
-            if (!Directory.Exists(androidPluginsPath)) {
+        if (!File.Exists(appManifestPath))
+        {
+            if (!Directory.Exists(androidPluginsPath))
+            {
                 Directory.CreateDirectory(androidPluginsPath);
             }
-            
+
             isAdjustManifestUsed = true;
             File.Copy(adjustManifestPath, appManifestPath);
 
             UnityEngine.Debug.Log("adjust: User defined AndroidManifest.xml file not found in Plugins/Android folder.");
             UnityEngine.Debug.Log("adjust: Creating default app's AndroidManifest.xml from AdjustAndroidManifest.xml file.");
-        } else {
+        }
+        else
+        {
             UnityEngine.Debug.Log("adjust: User defined AndroidManifest.xml file located in Plugins/Android folder.");
         }
 
         // If adjust manifest is used, we have already set up everything in it so that 
         // our native Android SDK can be used properly.
-        if (!isAdjustManifestUsed) {
+        if (!isAdjustManifestUsed)
+        {
             // However, if you already had your own AndroidManifest.xml, we'll now run
             // some checks on it and tweak it a bit if needed to add some stuff which
             // our native Android SDK needs so that it can run properly.
@@ -144,7 +156,8 @@ public class AdjustEditor {
         }
     }
 
-    private static void AddPermissions(XmlDocument manifest) {
+    private static void AddPermissions(XmlDocument manifest)
+    {
         // The adjust SDK needs two permissions to be added to you app's manifest file:
         // <uses-permission android:name="android.permission.INTERNET" />
         // <uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />
@@ -157,12 +170,18 @@ public class AdjustEditor {
         XmlElement manifestRoot = manifest.DocumentElement;
 
         // Check if permissions are already there.
-        foreach (XmlNode node in manifestRoot.ChildNodes) {
-            if (node.Name == "uses-permission") {
-                foreach (XmlAttribute attribute in node.Attributes) {
-                    if (attribute.Value.Contains("android.permission.INTERNET")) {
+        foreach (XmlNode node in manifestRoot.ChildNodes)
+        {
+            if (node.Name == "uses-permission")
+            {
+                foreach (XmlAttribute attribute in node.Attributes)
+                {
+                    if (attribute.Value.Contains("android.permission.INTERNET"))
+                    {
                         hasInternetPermission = true;
-                    } else if (attribute.Value.Contains("android.permission.ACCESS_WIFI_STATE")) {
+                    }
+                    else if (attribute.Value.Contains("android.permission.ACCESS_WIFI_STATE"))
+                    {
                         hasAccessWifiStatePermission = true;
                     }
                 }
@@ -170,29 +189,34 @@ public class AdjustEditor {
         }
 
         // If android.permission.INTERNET permission is missing, add it.
-        if (!hasInternetPermission) {
+        if (!hasInternetPermission)
+        {
             XmlElement element = manifest.CreateElement("uses-permission");
             element.SetAttribute("android__name", "android.permission.INTERNET");
             manifestRoot.AppendChild(element);
-
             UnityEngine.Debug.Log("adjust: android.permission.INTERNET permission successfully added to your app's AndroidManifest.xml file.");
-        } else {
+        }
+        else
+        {
             UnityEngine.Debug.Log("adjust: Your app's AndroidManifest.xml file already contains android.permission.INTERNET permission.");
         }
 
         // If android.permission.ACCESS_WIFI_STATE permission is missing, add it.
-        if (!hasAccessWifiStatePermission) {
+        if (!hasAccessWifiStatePermission)
+        {
             XmlElement element = manifest.CreateElement("uses-permission");
             element.SetAttribute("android__name", "android.permission.ACCESS_WIFI_STATE");
             manifestRoot.AppendChild(element);
-
             UnityEngine.Debug.Log("adjust: android.permission.ACCESS_WIFI_STATE permission successfully added to your app's AndroidManifest.xml file.");
-        } else {
+        }
+        else
+        {
             UnityEngine.Debug.Log("adjust: Your app's AndroidManifest.xml file already contains android.permission.ACCESS_WIFI_STATE permission.");
         }
     }
 
-    private static void AddBroadcastReceiver(XmlDocument manifest) {
+    private static void AddBroadcastReceiver(XmlDocument manifest)
+    {
         // We're looking for existance of broadcast receiver in the AndroidManifest.xml
         // Check out the example below how that usually looks like:
 
@@ -228,18 +252,20 @@ public class AdjustEditor {
         XmlNode applicationNode = null;
 
         // Let's find the application node.
-        foreach(XmlNode node in manifestRoot.ChildNodes) {
-            if (node.Name == "application") {
+        foreach(XmlNode node in manifestRoot.ChildNodes)
+        {
+            if (node.Name == "application")
+            {
                 applicationNode = node;
                 break;
             }
         }
 
         // If there's no applicatio node, something is really wrong with your AndroidManifest.xml.
-        if (applicationNode == null) {
+        if (applicationNode == null)
+        {
             UnityEngine.Debug.LogError("adjust: Your app's AndroidManifest.xml file does not contain \"<application>\" node.");
             UnityEngine.Debug.LogError("adjust: Unable to add the adjust broadcast receiver to AndroidManifest.xml.");
-
             return;
         }
 
@@ -249,42 +275,56 @@ public class AdjustEditor {
         // If not, add the adjust broadcast receiver to the manifest file.
         bool isThereAnyCustomBroadcastReiver = false;
 
-        foreach (XmlNode node in applicationNode.ChildNodes) {
-            if (node.Name == "receiver") {
-                foreach (XmlNode subnode in node.ChildNodes) {
-                    if (subnode.Name == "intent-filter") {
-                        foreach (XmlNode subsubnode in subnode.ChildNodes) {
-                            if (subsubnode.Name == "action") {
-                                foreach(XmlAttribute attribute in subsubnode.Attributes) {
-                                    if (attribute.Value.Contains("INSTALL_REFERRER")) {
+        foreach (XmlNode node in applicationNode.ChildNodes)
+        {
+            if (node.Name == "receiver")
+            {
+                foreach (XmlNode subnode in node.ChildNodes)
+                {
+                    if (subnode.Name == "intent-filter")
+                    {
+                        foreach (XmlNode subsubnode in subnode.ChildNodes)
+                        {
+                            if (subsubnode.Name == "action")
+                            {
+                                foreach (XmlAttribute attribute in subsubnode.Attributes)
+                                {
+                                    if (attribute.Value.Contains("INSTALL_REFERRER"))
+                                    {
                                         isThereAnyCustomBroadcastReiver = true;
                                         break;
                                     }
                                 }
                             }
 
-                            if (isThereAnyCustomBroadcastReiver) {
+                            if (isThereAnyCustomBroadcastReiver)
+                            {
                                 break;
                             }
                         }
                     }
 
-                    if (isThereAnyCustomBroadcastReiver) {
+                    if (isThereAnyCustomBroadcastReiver)
+                    {
                         break;
                     }
                 }
             }
 
-            if (isThereAnyCustomBroadcastReiver) {
+            if (isThereAnyCustomBroadcastReiver)
+            {
                 break;
             }
         }
 
         // Let's see what we have found so far.
-        if (isThereAnyCustomBroadcastReiver) {
+        if (isThereAnyCustomBroadcastReiver)
+        {
             UnityEngine.Debug.Log("adjust: It seems like you are using your own broadcast receiver.");
             UnityEngine.Debug.Log("adjust: Please, add the calls to the adjust broadcast receiver like described in here: https://github.com/adjust/android_sdk/blob/master/doc/english/referrer.md");
-        } else {
+        }
+        else
+        {
             // Generate adjust broadcast receiver entry and add it to the application node.
             XmlElement receiverElement = manifest.CreateElement("receiver");
             receiverElement.SetAttribute("android__name", "com.adjust.sdk.AdjustReferrerReceiver");
@@ -302,7 +342,8 @@ public class AdjustEditor {
         }
     }
 
-    private static void CleanManifestFile(String manifestPath) {
+    private static void CleanManifestFile(String manifestPath)
+    {
         // Due to XML writing issue with XmlElement methods which are unable
         // to write "android:[param]" string, we have wrote "android__[param]" string instead.
         // Now make the replacement: "android:[param]" -> "android__[param]"
@@ -319,10 +360,10 @@ public class AdjustEditor {
         manifestWriter.Close();
     }
 
-    private static int GetUnityIdeVersion() {
+    private static int GetUnityIdeVersion()
+    {
         int unityVersion;
         Int32.TryParse(Application.unityVersion[0].ToString(), out unityVersion);
-
         return unityVersion;
     }
 }
