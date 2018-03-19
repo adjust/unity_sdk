0499fd6bc1b520f4c795a747e75f7652982294fa
uerceg <ugi@adjust.com>
2018-03-19 12:47:13 +0100

    New version 4.12.5

--- Assets/Adjust/Android/AdjustAndroid.cs
+++ Assets/Adjust/Android/AdjustAndroid.cs
@@ -9,7 +9,7 @@ namespace com.adjust.sdk
 #if UNITY_ANDROID
     public class AdjustAndroid
     {
-        private const string sdkPrefix = "unity4.12.4";
+        private const string sdkPrefix = "unity4.12.5";
 
         private static bool launchDeferredDeeplink = true;
 
