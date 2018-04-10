4ac8c43176a28851e0f494b35424696604c946a3
nonelse <pedro@adjust.com>
2018-04-10 11:51:41 +0200

    Initialize android test library on contructor

--- Assets/Adjust/Test/TestFactoryAndroid.cs
+++ Assets/Adjust/Test/TestFactoryAndroid.cs
@@ -13,56 +13,35 @@ namespace com.adjust.sdk.test
             _baseUrl = baseUrl;
             CommandExecutor commandExecutor = new CommandExecutor(this, baseUrl);
             onCommandReceivedListener = new CommandListenerAndroid(commandExecutor);
+			ajoTestLibrary = new AndroidJavaObject(
+				"com.adjust.testlibrary.TestLibrary",
+				_baseUrl,
+				onCommandReceivedListener);
+			
         }
 
         public void StartTestSession()
         {
-            TestApp.Log("TestFactory -> StartTestSession()");
-
-            if (ajoTestLibrary == null)
-            {
-                ajoTestLibrary = new AndroidJavaObject(
-                    "com.adjust.testlibrary.TestLibrary",
-                    _baseUrl,
-                    onCommandReceivedListener);
-            }
-
-            TestApp.Log("TestFactory -> calling testLib.startTestSession()");
             ajoTestLibrary.Call("startTestSession", TestApp.CLIENT_SDK);
         }
 
         public void AddInfoToSend(string key, string paramValue)
         {
-            if (ajoTestLibrary == null)
-            {
-                return;
-            }
             ajoTestLibrary.Call("addInfoToSend", key, paramValue);
         }
 
         public void SendInfoToServer(string basePath)
         {
-            if (ajoTestLibrary == null)
-            {
-                return;
-            }
             ajoTestLibrary.Call("sendInfoToServer", basePath);
         }
 
 		public void AddTest(string testName)
 		{
-			if (ajoTestLibrary == null) {
-                return;
-            }
 			ajoTestLibrary.Call("addTest", testName);
 		}
 
 		public void AddTestDirectory(string testDirectory)
 		{
-			if (ajoTestLibrary == null)
-            {
-                return; 
-            }
 			ajoTestLibrary.Call("addTestDirectory", testDirectory);
 		}
     }
