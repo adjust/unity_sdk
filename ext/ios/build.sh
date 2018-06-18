78d11708ca226929ad55fba61fea53147b7ec2a1
uerceg <ugi@adjust.com>
2018-06-18 08:06:56 +0200

    iOS and Android build scripts update

--- ext/ios/build.sh
+++ ext/ios/build.sh
@@ -2,16 +2,40 @@
 
 set -e
 
+# ======================================== #
+
+# Colors for output
+NC='\033[0m'
+RED='\033[0;31m'
+CYAN='\033[1;36m'
+GREEN='\033[0;32m'
+
+# ======================================== #
+
+# Set directories of interest for the script
 SRCDIR=./sdk
 LIBOUTDIR=../../Assets/Adjust/iOS
-
 SDK_STATIC_FRAMEWORK=$SRCDIR/Frameworks/Static/AdjustSdk.framework
 TEST_STATIC_FRAMEWORK=$SRCDIR/Frameworks/Static/AdjustTestLibrary.framework
 
+# ======================================== #
+
+echo -e "${CYAN}[ADJUST][IOS][BUILD]:${GREEN} Building AdjustStatic framework target ... ${NC}"
 (cd $SRCDIR; xcodebuild -target AdjustStatic -configuration Release clean build)
 cp -v $SDK_STATIC_FRAMEWORK/Versions/A/AdjustSdk $LIBOUTDIR/AdjustSdk.a
 cp -v $SDK_STATIC_FRAMEWORK/Versions/A/Headers/* $LIBOUTDIR
+echo -e "${CYAN}[ADJUST][IOS][BUILD]:${GREEN} Done! ${NC}"
 
+# ======================================== #
+
+echo -e "${CYAN}[ADJUST][IOS][BUILD]:${GREEN} Building AdjustTestLibraryStatic framework target ... ${NC}"
 (cd $SRCDIR/AdjustTests/AdjustTestLibrary; xcodebuild -target AdjustTestLibraryStatic -configuration Release clean build)
 cp -v $TEST_STATIC_FRAMEWORK/Versions/A/AdjustTestLibrary $LIBOUTDIR/AdjustTestLibrary.a
 cp -v $TEST_STATIC_FRAMEWORK/Versions/A/Headers/* $LIBOUTDIR
+echo -e "${CYAN}[ADJUST][IOS][BUILD]:${GREEN} Done! ${NC}"
+
+# ======================================== #
+
+echo -e "${CYAN}[ADJUST][IOS][BUILD]:${GREEN} Script completed! ${NC}"
+
+# ======================================== #
