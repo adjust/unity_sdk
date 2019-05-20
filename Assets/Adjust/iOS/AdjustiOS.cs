14c9133cf05377a672de2e0d32462bed05ade2ac
uerceg <ugi@adjust.com>
2019-05-20 12:17:36 +0200

    New version 4.18.0

--- Assets/Adjust/iOS/AdjustiOS.cs
+++ Assets/Adjust/iOS/AdjustiOS.cs
@@ -8,7 +8,7 @@ namespace com.adjust.sdk
 #if UNITY_IOS
     public class AdjustiOS
     {
-        private const string sdkPrefix = "unity4.17.2";
+        private const string sdkPrefix = "unity4.18.0";
 
         [DllImport("__Internal")]
         private static extern void _AdjustLaunchApp(
