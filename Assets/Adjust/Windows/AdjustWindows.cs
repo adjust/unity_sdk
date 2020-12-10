337b69b865d9a7de266683ad87821e0dcecae75e
ugi <ugi@adjust.com>
2020-12-10 22:26:18 +0100

    update version number to 4.24.0

--- Assets/Adjust/Windows/AdjustWindows.cs
+++ Assets/Adjust/Windows/AdjustWindows.cs
@@ -17,7 +17,7 @@ namespace com.adjust.sdk
 {
     public class AdjustWindows
     {
-        private const string sdkPrefix = "unity4.23.2";
+        private const string sdkPrefix = "unity4.24.0";
         private static bool appLaunched = false;
 
         public static void Start(AdjustConfig adjustConfig)
--- doc/english/migration/migrate.md
+++ doc/english/migration/migrate.md
@@ -1,4 +1,4 @@
-## Migrate your Adjust SDK for Unity3d to 4.23.2 from 3.4.4
+## Migrate your Adjust SDK for Unity3d to 4.24.0 from 3.4.4
 
 ### Migration procedure
 
