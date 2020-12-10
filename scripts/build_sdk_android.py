ef4ab7179ad6df25f26c6caa484bac638d765f7f
ugi <ugi@adjust.com>
2020-12-10 22:09:57 +0100

    add adjust-android-oaid.jar regeneration to build scripts

--- scripts/build_sdk_android.py
+++ scripts/build_sdk_android.py
@@ -3,9 +3,11 @@ from scripting_utils import *
 def build(root_dir, android_submodule_dir, with_test_lib):
     # ------------------------------------------------------------------
     # Paths.
-    src_dir        = '{0}/sdk/Adjust'.format(android_submodule_dir)
-    jar_in_dir     = '{0}/sdk-core/build/libs'.format(src_dir)
-    jar_out_dir    = '{0}/Assets/Adjust/Android'.format(root_dir)
+    src_dir             = '{0}/sdk/Adjust'.format(android_submodule_dir)
+    jar_in_dir          = '{0}/sdk-core/build/libs'.format(src_dir)
+    jar_in_dir_oaid     = '{0}/sdk-plugin-oaid/build/libs'.format(src_dir)
+    jar_out_dir         = '{0}/Assets/Adjust/Android'.format(root_dir)
+    jar_out_dir_oaid    = '{0}/Assets/AdjustOaid/Android'.format(root_dir)
 
     # ------------------------------------------------------------------
     # Building Android SDK JAR in release mode.
@@ -17,6 +19,16 @@ def build(root_dir, android_submodule_dir, with_test_lib):
     # Copy Android SDK JAR to destination.
     debug_green('Copying generated Android SDK JAR to destination ...')
     copy_file('{0}/adjust-sdk-release.jar'.format(jar_in_dir), '{0}/adjust-android.jar'.format(jar_out_dir))
+
+    # ------------------------------------------------------------------
+    # Building Android SDK JAR in release mode.
+    debug_green('Building Android OAID plugin JAR in release mode ...')
+    gradle_make_oaid_jar_release()
+
+    # ------------------------------------------------------------------
+    # Copy Android OAID plugin JAR to destination.
+    debug_green('Copying generated Android OAID plugin JAR to destination ...')
+    copy_file('{0}/sdk-plugin-oaid.jar'.format(jar_in_dir_oaid), '{0}/adjust-android-oaid.jar'.format(jar_out_dir_oaid))
 
     if with_test_lib:
         # ------------------------------------------------------------------
