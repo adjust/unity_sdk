6b08687564482641d7ba487fc2d925c35bf6c4ee
Evgeny Kozlov (Akvelon) <evgeny.kozlov@akvelon.com>
2021-05-18 14:36:00 +0300

    fix: Applied formatting on CustomAdjustEditor

--- Assets/Adjust/Editor/CustomAdjustEditor.cs
+++ Assets/Adjust/Editor/CustomAdjustEditor.cs
@@ -2,43 +2,44 @@
 using UnityEngine;
 using UnityEditor;
 
-namespace com.adjust.sdk {
-	[CustomEditor(typeof(Adjust))]
-	public class CustomAdjustEditor : Editor
-	{
-		List<string> deeplinkingParameters;
-		void OnEnable()
-		{
-			deeplinkingParameters = AdjustSettings.UrlSchemes;
-		}
+namespace com.adjust.sdk
+{
+    [CustomEditor(typeof(Adjust))]
+    public class CustomAdjustEditor : Editor
+    {
+        List<string> deeplinkingParameters;
+        void OnEnable()
+        {
+            deeplinkingParameters = AdjustSettings.UrlSchemes;
+        }
 
-		public override void OnInspectorGUI()
-		{
-			base.OnInspectorGUI();
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
 
-			EditorGUILayout.TextField("User Tracking Usage Description", AdjustSettings.UserTrackingUsageDescription);
+            EditorGUILayout.TextField("User Tracking Usage Description", AdjustSettings.UserTrackingUsageDescription);
 
-			GUILayout.Space(20f);
-			GUILayout.BeginHorizontal();
-			GUILayout.Label("Add URL schemes for deeplinking", EditorStyles.boldLabel);
-			if (GUILayout.Button("+"))
-			{
-				deeplinkingParameters.Add("");
-			}
-			if (GUILayout.Button("-"))
-			{
-				deeplinkingParameters.RemoveAt(deeplinkingParameters.Count - 1);
-			}
-			GUILayout.EndHorizontal();
+            GUILayout.Space(20f);
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Add URL schemes for deeplinking", EditorStyles.boldLabel);
+            if (GUILayout.Button("+"))
+            {
+                deeplinkingParameters.Add("");
+            }
+            if (GUILayout.Button("-"))
+            {
+                deeplinkingParameters.RemoveAt(deeplinkingParameters.Count - 1);
+            }
+            GUILayout.EndHorizontal();
 
-			for (int i = 0; i < deeplinkingParameters.Count; i++)
-			{
-				string newKeyProperty = deeplinkingParameters[i];
-				deeplinkingParameters[i] = EditorGUILayout.TextField("New Deeplink " + i, newKeyProperty);
-				EditorGUILayout.Space();
-			}
-			
-			serializedObject.ApplyModifiedProperties();
-		}
-	}
+            for (int i = 0; i < deeplinkingParameters.Count; i++)
+            {
+                string newKeyProperty = deeplinkingParameters[i];
+                deeplinkingParameters[i] = EditorGUILayout.TextField("New Deeplink " + i, newKeyProperty);
+                EditorGUILayout.Space();
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
 }
