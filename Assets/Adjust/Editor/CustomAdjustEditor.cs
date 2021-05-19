df37e17c735ee29d10b73c6e4694b86c6c31d4d1
Evgeny Kozlov (Akvelon) <evgeny.kozlov@akvelon.com>
2021-05-19 13:07:14 +0300

    fix: Separated creating of list manipulation GUI to the function

--- Assets/Adjust/Editor/CustomAdjustEditor.cs
+++ Assets/Adjust/Editor/CustomAdjustEditor.cs
@@ -9,6 +9,7 @@ namespace com.adjust.sdk
     {
         private List<string> deeplinkingParameters;
         private List<string> universalLinkDomains;
+
         void OnEnable()
         {
             deeplinkingParameters = AdjustSettings.UrlSchemes;
@@ -25,51 +26,39 @@ namespace com.adjust.sdk
             AdjustSettings.UrlSchemesDeepLinksEnabled = GUILayout.Toggle(AdjustSettings.UrlSchemesDeepLinksEnabled, "URL schemes deep links enabled");
             if (AdjustSettings.UrlSchemesDeepLinksEnabled)
             {
-                GUILayout.BeginHorizontal();
-                GUILayout.Label("Add URL schemes for deep linking", EditorStyles.boldLabel);
-                if (GUILayout.Button("+"))
-                {
-                    deeplinkingParameters.Add("");
-                }
-                if (GUILayout.Button("-"))
-                {
-                    deeplinkingParameters.RemoveAt(deeplinkingParameters.Count - 1);
-                }
-                GUILayout.EndHorizontal();
-
-                for (int i = 0; i < deeplinkingParameters.Count; i++)
-                {
-                    string newKeyProperty = deeplinkingParameters[i];
-                    deeplinkingParameters[i] = EditorGUILayout.TextField("URL scheme " + i, newKeyProperty);
-                    EditorGUILayout.Space();
-                }
+                CreateListManipulationUI("Add URL schemes for deep linking", "URL scheme ", deeplinkingParameters);
             }
 
             GUILayout.Space(20f);
             AdjustSettings.UniversalLinksEnabled = GUILayout.Toggle(AdjustSettings.UniversalLinksEnabled, "Universal links enabled");
             if (AdjustSettings.UniversalLinksEnabled)
             {
-                GUILayout.BeginHorizontal();
-                GUILayout.Label("Add domains for Universal linking", EditorStyles.boldLabel);
-                if (GUILayout.Button("+"))
-                {
-                    universalLinkDomains.Add("");
-                }
-                if (GUILayout.Button("-"))
-                {
-                    universalLinkDomains.RemoveAt(deeplinkingParameters.Count - 1);
-                }
-                GUILayout.EndHorizontal();
-
-                for (int i = 0; i < universalLinkDomains.Count; i++)
-                {
-                    string newKeyProperty = universalLinkDomains[i];
-                    universalLinkDomains[i] = EditorGUILayout.TextField("Domain " + i, newKeyProperty);
-                    EditorGUILayout.Space();
-                }
+                CreateListManipulationUI("Add domains for Universal linking", "Domain ", universalLinkDomains);
             }
 
             serializedObject.ApplyModifiedProperties();
+        }
+
+        private void CreateListManipulationUI(string listManipulationUIHeader, string listItemName, List<string> manipulatedList)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(listManipulationUIHeader, EditorStyles.boldLabel);
+            if (GUILayout.Button("+"))
+            {
+                manipulatedList.Add("");
+            }
+            if (GUILayout.Button("-"))
+            {
+                manipulatedList.RemoveAt(manipulatedList.Count - 1);
+            }
+            GUILayout.EndHorizontal();
+
+            for (int i = 0; i < manipulatedList.Count; i++)
+            {
+                string newKeyProperty = manipulatedList[i];
+                manipulatedList[i] = EditorGUILayout.TextField(listItemName + i, newKeyProperty);
+                EditorGUILayout.Space();
+            }
         }
     }
 }
