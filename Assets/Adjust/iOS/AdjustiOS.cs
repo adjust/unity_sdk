a7a572159185d0ba06ad13f03b91b92c7cc5b705
uerceg <ugi@adjust.com>
2023-09-06 13:26:50 +0200

    feat: update version number to 4.34.1

--- Assets/Adjust/iOS/AdjustiOS.cs
+++ Assets/Adjust/iOS/AdjustiOS.cs
@@ -8,7 +8,7 @@ namespace com.adjust.sdk
 #if UNITY_IOS
     public class AdjustiOS
     {
-        private const string sdkPrefix = "unity4.34.0";
+        private const string sdkPrefix = "unity4.34.1";
 
         [DllImport("__Internal")]
         private static extern void _AdjustLaunchApp(
