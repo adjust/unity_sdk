9c7f5964a9c0a1a1fe66822f3eb0dc02d0658560
uerceg <ugi@adjust.com>
2023-09-27 12:23:58 +0200

    feat: update version number to 4.35.0

--- Assets/Adjust/Windows/AdjustWindows.cs
+++ Assets/Adjust/Windows/AdjustWindows.cs
@@ -17,7 +17,7 @@ namespace com.adjust.sdk
 {
     public class AdjustWindows
     {
-        private const string sdkPrefix = "unity4.34.1";
+        private const string sdkPrefix = "unity4.35.0";
         private static bool appLaunched = false;
 
         public static void Start(AdjustConfig adjustConfig)
