1f97ce7c43b92c32d4901cedb73cf812fe18622f
uerceg <ugi@adjust.com>
2025-08-11 16:17:30 +0200

    feat: update version number to 5.4.2

--- Assets/Adjust/Scripts/AdjustiOS.cs
+++ Assets/Adjust/Scripts/AdjustiOS.cs
@@ -8,7 +8,7 @@ namespace AdjustSdk
 #if UNITY_IOS
     public class AdjustiOS
     {
-        private const string sdkPrefix = "unity5.4.1";
+        private const string sdkPrefix = "unity5.4.2";
 
         // app callbacks as method parameters
         private static List<Action<bool>> appIsEnabledGetterCallbacks;
