2357c8b23bcc91f0843a4b970fb9dc99c8f60e25
uerceg <ugi@adjust.com>
2026-04-17 10:00:26 +0200

    update version number to 5.6.0

--- Assets/Adjust/Scripts/AdjustiOS.cs
+++ Assets/Adjust/Scripts/AdjustiOS.cs
@@ -8,7 +8,7 @@ namespace AdjustSdk
 #if UNITY_IOS
     public class AdjustiOS
     {
-        private const string sdkPrefix = "unity5.5.1";
+        private const string sdkPrefix = "unity5.6.0";
 
         // app callbacks as method parameters
         private static List<Action<bool>> appIsEnabledGetterCallbacks;
